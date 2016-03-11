e4865c9b9df72d954e681cd57ebc0cad48a88062
mur1ll0 <murillomaleski@gmail.com>
2016-03-10 21:47:38 -0300

    Update Trabalho2.v

--- Trabalho2.v
+++ Trabalho2.v
@@ -1,25 +1,20 @@
 module BlinkLed(
 	input CLOCK_50,
-	input [3:0] KEY,
 	output [7:0] LEDG);
 	
 	reg [32:0] cont;
-	reg [7:0] state;
+	reg state;
 	
-	assign LEDG[0] = state[0];
+	assign LEDG[0] = state;
 	
-	always @ (posedge CLOCK_50 or posedge KEY[0]) begin
+	always @ (posedge CLOCK_50) begin
 		cont = cont + 1;
-		if (cont == 50000000 && state[0] == 0) begin
-			state[0] <= 1;
+		if (cont == 50000000 && state == 0) begin
+			state <= 1;
 			cont <= 0;
 		end
-		else if (cont == 50000000 && state[0] == 1) begin
-			state[0] <= 0;
-			cont <= 0;
-		end
-		if (KEY[0] == 1) begin
-			state[0] <= 0;
+		else if (cont == 50000000 && state == 1) begin
+			state <= 0;
 			cont <= 0;
 		end
 	end
