287c2bcd695dd17d8cf50446ab57e3cf41712cb1
mur1ll0 <murillomaleski@gmail.com>
2016-04-04 13:09:52 -0300

    Added files via upload

---
+++ Trabalho5/CompareOutputs.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    int outC, outV;
+    char xis;
+
+    FILE *cOut = fopen("Out.txt", "r");
+    FILE *vOut = fopen("VerilogOut.txt", "r");
+
+    if (cOut == NULL || vOut == NULL){
+        printf("Erro ao abrir arquivo.");
+        return 0;
+    }
+
+    while ((fscanf(vOut, "%d\n", &outV) != EOF)&& (fscanf(cOut, "%d\n", &outC) != EOF)){
+        if (outC != outV){
+            printf("\nSaidas diferentes\n%d %d\n", outC, outV);
+            return 0;
+        }
+    }
+    printf("\nSaidas Iguais\n");
+
+    fclose(vOut);
+    fclose(cOut);
+
+    return 0;
+}
