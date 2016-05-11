de4eea602b206dfd5a2b1c7bab2a3542e3670dd0
mur1ll0 <murillomaleski@gmail.com>
2016-05-11 16:03:28 -0300

    Update CompareOutputs.c

--- Trabalho5/CompareOutputs.c
+++ Trabalho5/CompareOutputs.c
@@ -4,7 +4,7 @@
 
 int main()
 {
-    int outC, outV;
+    int outC, outV, contador;
     char xis;
 
     FILE *cOut = fopen("Out.txt", "r");
@@ -15,16 +15,20 @@ int main()
         return 0;
     }
 
+    contador = 1;
     while ((fscanf(vOut, "%d\n", &outV) != EOF)&& (fscanf(cOut, "%d\n", &outC) != EOF)){
         if (outC != outV){
-            printf("\nSaidas diferentes\n%d %d\n", outC, outV);
+            printf("\nLinha %d\nSaidas diferentes\n%d %d\n", contador, outC, outV);
             return 0;
         }
+        contador++;
     }
     printf("\nSaidas Iguais\n");
 
     fclose(vOut);
     fclose(cOut);
+    
+    getchar();
 
     return 0;
 }
